d32c4c29770b2a1a0937d3586acda3d3219255a3
Alina <linarogozhnikova30@gmail.com>
2024-10-24 18:39:04 +0500

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,61 @@
-import HtmlWebpackPlugin from 'html-webpack-plugin';
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-export default {
-  mode: process.env.NODE_ENV || 'development',
-  module: {
-    rules: [
-      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
-      },
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+
+const config = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'public'),
+    },
+    devServer: {
+        open: true,
+        host: 'localhost',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'index.html',
+        }),
+
+        // Add your plugins here
+        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'index.html',
-    }),
-  ],
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/i,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+            {
+                test: /\.scss$/, // Регулярное выражение для файлов .scss
+                use: [
+                  'style-loader', // Вставляет CSS в DOM
+                  'css-loader',   // Понимает @import и url() как import/require()
+                  'sass-loader',
+                  'postcss-loader'  // Компилирует Sass в CSS
+                ]
+              }
+
+            // Add your rules for custom modules here
+            // Learn more about loaders from https://webpack.js.org/loaders/
+        ],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+        
+        
+    } else {
+        config.mode = 'development';
+    }
+    return config;
 };
