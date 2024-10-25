ae3f43a093828df2a19aa1ee28d6f64497d0af58
Alina <linarogozhnikova30@gmail.com>
2024-10-26 00:13:59 +0500

    fixed input

--- src/view.js
+++ src/view.js
@@ -126,7 +126,7 @@ const handleProcessState = (processState, elements, i18n) => {
       elements.button.textContent = 'Добавить';
       elements.form.reset();
       elements.form.focus();
-      elements.input.click();
+      elements.input.focus();
       elements.feedbackContainer.classList.remove('text-danger');
       elements.feedbackContainer.classList.add('text-success');
       elements.feedbackContainer.textContent = i18n.t('form.success');
